e298e8918563c31f2b35317f3a2fdacca3195701
gideonclottey <gideonclotteyint@gmail.com>
2024-11-01 12:51:47 +0000

    still more on lisst

---
+++ hogwarts.py
@@ -0,0 +1,17 @@
+students = ['Hermoine', 'Harry','Ron']
+print(students[0])
+
+#using loop
+
+students = ['Hermoine', 'Harry','Ron']
+
+for student in students:
+    print(student)
+
+
+#using the len function 
+
+students = ['Hermoine', 'Harry','Ron']
+
+for i in range(len(students)):
+    print( i+1,students[i])
